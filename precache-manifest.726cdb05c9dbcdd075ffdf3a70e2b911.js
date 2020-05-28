b0518d0fb03c23b8b5926e7fd96eace564223682
mohammad taheri <mohammadtaheri30132@gmail.com>
2020-05-28 03:30:25 -0700

    Updates

---
+++ precache-manifest.726cdb05c9dbcdd075ffdf3a70e2b911.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e104071585b90485d4f0301fa6f6283",
+    "url": "/todo-add/index.html"
+  },
+  {
+    "revision": "2e8f14daac076dd1ae0a",
+    "url": "/todo-add/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "0afdf5d42ab38a7c04ee",
+    "url": "/todo-add/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e8f14daac076dd1ae0a",
+    "url": "/todo-add/static/js/2.6e06df78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-add/static/js/2.6e06df78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0afdf5d42ab38a7c04ee",
+    "url": "/todo-add/static/js/main.8dba4adf.chunk.js"
+  },
+  {
+    "revision": "95e2278c9a6354bfbf18",
+    "url": "/todo-add/static/js/runtime-main.b2b49c8d.js"
+  }
+]);
